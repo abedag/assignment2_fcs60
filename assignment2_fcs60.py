733e15d866ba54c69ccdcf9198b6fd2effd581d0
abedag <abedalrahman7117@gmail.com>
2025-02-18 21:50:51 +0200

    Add <> Adding input conditions

--- assignment2_fcs60.py
+++ assignment2_fcs60.py
@@ -3,3 +3,10 @@ print("Nabiha's Salary")
 months = ["january","february","march","april","may","june","july",
 "august","september","october","november","december"]
 
+month_sal = int(input("Enter the salary of the month in $: "))
+month_name = input("Enter the name of the month: ")
+
+print("Enter the percentage of each of the following payments:")
+inp_savings = int(input("Savings:"))
+inp_rent = int(input("Rent:"))
+inp_electricity = int(input("Electricity:"))
