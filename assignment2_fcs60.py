be68c3e0e9d79904b944f2f20a5cc0fe975c0ec8
abedag <abedalrahman7117@gmail.com>
2025-02-18 22:07:50 +0200

    Add <> Adding more monthly and yearly payment calculations

--- assignment2_fcs60.py
+++ assignment2_fcs60.py
@@ -14,3 +14,8 @@ inp_electricity = int(input("Electricity:"))
 savings = float((inp_savings / 100) * month_sal)
 rent = float((inp_rent / 100) * month_sal)
 electricity = float((inp_electricity / 100) * month_sal)
+
+total_paym = savings + rent + electricity
+sal_remainder = month_sal - total_paym 
+year_rent_elec = (rent + electricity) * len(months)
+sal_squared = month_sal ** 2
